d3a5e37142d24d01712484510b9d197b2bee7d69
Frederic Feliciane <fredericfeliciane8@gmail.com>
2023-09-03 23:16:43 +0400

    test iframe added

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,5 +1,24 @@
 const Test = () => {
-  return <h1>Test</h1>;
+  return (
+    <>
+      <h1>Test</h1>;
+      <h1>Test</h1>;
+
+      <div>
+        <h1>My React Iframe Component</h1>
+        <iframe
+        src="https://example.com"
+        width="200"
+        height="300"
+        style={{border: "1px solid black"}}>
+      </iframe>
+      </div>
+
+
+    </>
+  )
+
+
 };
 
 export default Test;
